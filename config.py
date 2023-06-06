b02399ec7fb9e3bc0ac3a49e210242508436f9dc
Toru Hishinuma <toru.hishinuma@proxima-ai-tech.com>
2023-06-06 11:30:44 +0900

    pendulum:hidden=32でも標準vaeで良い方策が求まってしまう

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ if cfg_env == "pendulum":
     cfg_z_dim = 1
     cfg_alpha = 0.1 # 0.3だと学習が発散?　0.01だと発散しない
     cfg_sac_num_steps = 100000
-    cfg_dec_hidden=48 # pendulum: 1layer32unitは惜しい, 2layer32unitは重み無しで学習できてしまう, 2layer16unitも惜しい, 3layer16unitはアリ
+    cfg_dec_hidden=32 
 
 if cfg_env == "cartpole":
     cfg_z_dim = 2
@@ -13,8 +13,8 @@ if cfg_env == "cartpole":
     cfg_sac_num_steps = 200000
     cfg_dec_hidden=64
 
-# cfg_seed = 0
-cfg_seed = 1
+cfg_seed = 0
+# cfg_seed = 1
 cfg_default_lr=5e-4
 cfg_default_early=100
 
