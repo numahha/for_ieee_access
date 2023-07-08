f108ee7cbd7e2d6a85a5caad5da3391b446e6311
Toru Hishinuma <toru.hishinuma@proxima-ai-tech.com>
2023-07-08 12:08:36 +0900

    美酒正2 seed3 ok iter1and2

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ if cfg_env == "cartpole":
     cfg_dec_hidden=64
 
 # cfg_seed = 0
-cfg_seed = 1
+# cfg_seed = 1
 # cfg_seed = 2
-# cfg_seed = 3
+cfg_seed = 3
 # cfg_seed = 4
 cfg_default_lr=5e-4
 cfg_default_early=100
