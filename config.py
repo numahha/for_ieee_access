9766dff7f6831e19b396133c324f3e6f02c79788
Toru Hishinuma <toru.hishinuma@proxima-ai-tech.com>
2023-07-22 17:12:33 +0900

    cartpole seed1 iter1 ok

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-cfg_env = 'pendulum'
-# cfg_env = 'cartpole'
+# cfg_env = 'pendulum'
+cfg_env = 'cartpole'
 
 if cfg_env == "pendulum":
     cfg_z_dim = 1
@@ -9,16 +9,16 @@ if cfg_env == "pendulum":
 
 if cfg_env == "cartpole":
     cfg_z_dim = 2
-    cfg_alpha = 0.2
+    cfg_alpha = 0.1
     cfg_sac_num_steps = 200000
     cfg_dec_hidden=64
 
 # cfg_seed = 0
-# cfg_seed = 1
+cfg_seed = 1
 # cfg_seed = 2
 # cfg_seed = 3
 # cfg_seed = 4
-cfg_seed = 5
+# cfg_seed = 5
 cfg_default_lr=5e-4
 cfg_default_early=100
 cfg_policy_evaluation_maxiter=5
